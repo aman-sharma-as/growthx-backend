9ee93739a9ed218fd939562e3f85052def194d4c
ShivaG103 <shvm5454@gmail.com>
2024-03-11 16:49:28 +0530

    home route added

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 4000;
 
 dbConnect();
 
+
+app.get('/', (req, res) => {
+  res.send("<h1>HomePage</h1>")
+})
+
 app.listen(PORT, () => {
   console.log(`App is started at ${PORT}`);
 });
