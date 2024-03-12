ce1914febae4e75bf98e3998528a73dc6689205e
aman-sharma-as <aman.745500@gmail.com>
2024-03-12 15:14:12 +0530

    User Model is added

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  employeeID: {
+    type: String,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["Manager", "Employee", "Intern"],
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+  approve: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
